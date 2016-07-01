a17fa4d80c142866d258d2435d97d3c2fd3f3f9e
罗传月武 <luochuanyuewu@gmail.com>
2016-07-01 21:53:02 +0800

    加入route文件的注释

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,24 +2,23 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| 应用程序路由
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
+| 此文件用来定义整个应用的路由信息
+| 路由就是，你输入了什么url，定义返回什么样的资源
 */
 
+//根目录路由，返回welcome界面并追加所有公开的帖子数据
 Route::get('/', function () {
     $notes = \App\Note::where('is_public', 1)->orderBy('created_at', 'desc')->get();
     return view('welcome', compact('notes'));
 });
-
+//返回readme界面
 Route::get('/readme', function () {
     return view('readme');
 });
-
+//返回显示所有用户的界面
 Route::get('user/showall', ['as' => 'user.showall', function () {
 
     $users = \App\User::all();
@@ -33,30 +32,38 @@ Route::get('note/show/{id}', ['as' => 'note.show', 'uses' => "NoteController@sho
 Route::auth();
 
 
-//Route::get('/home', 'HomeController@index');
-
+//这是路由组，在这个组下的所有界面都必须登陆了才能访问
 Route::group(['middleware' => ['auth']], function () {
 
     //Note相关的route
-
+    //查看登陆用户的所有帖子
     Route::get('note', ['as' => 'note.index', 'uses' => "NoteController@index"]);
+    //编辑登陆用户的某个帖子
     Route::get('note/edit/{id}', ['as' => 'note.edit', 'uses' => "NoteController@edit"]);
+    //处理登陆用户的某个帖子的公开性
     Route::get('note/setPublic/{id}', ['as' => 'note.setPublic', 'uses' => "NoteController@setPublic"]);
-
+    //用于处理创建某个帖子
     Route::post('note/store', ['as' => 'note.store', 'uses' => "NoteController@store"]);
+    //用于更新某个帖子
     Route::patch('note/update/{id}', ['as' => 'note.update', 'uses' => "NoteController@update"]);
-
+    //用于删除某个帖子
     Route::get('note/destroy/{id}', ['as' => 'note.destroy', 'uses' => "NoteController@destroy"]);
 
     //Tag相关的route
+    //用于显示所有的分类
     Route::get('category', ['as' => 'category.index', 'uses' => "CategoryController@index"]);
+    //用于按照特定分类来浏览帖子
     Route::get('category/show/{id}', ['as' => 'category.show', 'uses' => "CategoryController@show"]);
+    //新建某个分类
     Route::post('category/store', ['as' => 'category.store', 'uses' => "CategoryController@store"]);
+    //删除某个分类
     Route::get('category/destroy/{id}', ['as' => 'category.destroy', 'uses' => "CategoryController@destroy"]);
 
 
     //User相关的route
+    //查看某个用户
     Route::get('user/edit', ['as' => 'user.edit', 'uses' => "UserController@edit"]);
+    //编辑某个用户
     Route::patch('user/update', ['as' => 'user.update', 'uses' => "UserController@update"]);
 
 });
